61557ad982b4c7f23f19f6452472825b68d26d46
Joshua de wet <ST10313014@vcconnect.edu.za>
2024-10-10 13:37:07 +0200

    First Commit
    Created app.

---
+++ ABCRetail_ST10313014_CLDPOEFinal/ABCRetail_ST10313014_CLDPOEFinal/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABCRetail_ST10313014_CLDPOEFinal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ABCRetail_ST10313014_CLDPOEFinal/ABCRetail_ST10313014_CLDPOEFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ABCRetail_ST10313014_CLDPOEFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
